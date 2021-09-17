d4aad5c447f9a8fbc49efca49fa34bdd4330ea80
Paulo Nakashima <p.nakashima@hotmail.com>
2021-09-16 23:38:56 -0300

    add states to form

--- src/components/StudentForm/index.jsx
+++ src/components/StudentForm/index.jsx
@@ -1,25 +1,99 @@
+import { useState } from "react"
+
 
 const StudentForm = () => {
 
+    const [name, setName] = useState("")
+    const [dob, setDob] = useState("")
+    const [responsible, setResponsible] = useState("")
+    const [responsiblePhone, setResponsiblePhone] = useState("")
+    const [emergency, setEmergency] = useState("")
+    const [emergencyPhone, setEmergencyPhone] = useState("")
+    const [foodRestriction, setFoodRestriction] = useState("")
+    const [restrictionDescription, setRestrictionDescription] = useState("")
+    const [photoAuth, setPhotoAuth] = useState("")
+    const [authorizedPeople, setAuthorizedPeople] = useState("")
+    const [classNumber, setClassNumber] = useState("")
+    const [additionalObs, setAdditionalObs] = useState("")
+
+
+    const phoneMask = (event) => {
+        let text = event.target.value
+        let numbers = text.replace(/\D/g, '')
+        let mask = [...numbers].map((letter, i) => {
+            if (i === 0) return ['(', letter]
+            if (i === 2) return [')', letter]
+            if (i === 7) return ['-', letter]
+            if (i > 10) return ['']
+            return letter
+        }).flat(1).join('')
+        const inputValue = mask
+        event.target.value = mask
+        return inputValue
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const student = {
+            name,
+            dob,
+            responsible,
+            responsiblePhone,
+            emergency,
+            emergencyPhone,
+            foodRestriction,
+            restrictionDescription,
+            photoAuth,
+            authorizedPeople,
+            classNumber,
+            additionalObs
+        }
+        console.log(student)
+    }
+
     return (
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="student-form">
 
                 <label htmlFor="name">Nome:</label>
-                <input type="text" id="name" name="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={(e) => setName(e.target.value)}
+                />
 
                 <label htmlFor="dob">Data de nascimento:</label>
-                <input type="date" id="dob" name="dob" />
+                <input
+                    type="date"
+                    id="dob"
+                    name="dob"
+                    onChange={(e) => setDob(e.target.value)}
+                />
 
                 <label htmlFor="responsible">Responsável:</label>
-                <input type="text" id="responsible" name="responsible" />
+                <input
+                    type="text"
+                    id="responsible"
+                    name="responsible"
+                    onChange={(e) => setResponsible(e.target.value)}
+                />
 
                 <label htmlFor="responsiblePhone">Telefone do responsável:</label>
-                <input type="text" id="responsiblePhone" name="responsiblePhone" />
+                <input
+                    type="text"
+                    id="responsiblePhone"
+                    name="responsiblePhone"
+                    onChange={(e) => setResponsiblePhone(phoneMask(e))}
+                />
 
                 <label htmlFor="emergency">Contato de emergência:</label>
-                <select name="emergency" value="">
+                <select
+                    name="emergency"
+                    value={emergency}
+                    onChange={(e) => setEmergency(e.target.value)}
+                >
                     <option value="none"></option>
                     <option value="pais">Pais</option>
                     <option value="tios">Tios</option>
@@ -28,26 +102,55 @@ const StudentForm = () => {
                 </select>
 
                 <label htmlFor="emergencyPhone">Telefone para emergências:</label>
-                <input type="text" id="emergencyPhone" name="emergencyPhone" />
+                <input
+                    type="text"
+                    id="emergencyPhone"
+                    name="emergencyPhone"
+                    onChange={(e) => setEmergencyPhone(phoneMask(e))}
+                />
 
                 <div>
-                <input type="checkbox" id="foodRestriction" name="foodRestriction" checked />
-                <label for="foodRestriction">Possui restrição alimentar</label>
+                    <input
+                        type="checkbox"
+                        id="foodRestriction"
+                        name="foodRestriction"
+                        onChange={(e) => setFoodRestriction(e.target.value)}
+                    />
+                    <label htmlFor="foodRestriction">Possui restrição alimentar</label>
                 </div>
 
                 <label htmlFor="restrictionDescription">Descrição das restrições alimentares:</label>
-                <input type="text" id="restrictionDescription" name="restrictionDescription" />
+                <input
+                    type="text"
+                    id="restrictionDescription"
+                    name="restrictionDescription"
+                    onChange={(e) => setRestrictionDescription(e.target.value)}
+                />
 
                 <div>
-                <input type="checkbox" id="photoAuth" name="photoAuth" checked />
-                <label for="photoAuth">Autorização de fotos e vídeos da criança</label>
+                    <input
+                        type="checkbox"
+                        id="photoAuth"
+                        name="photoAuth"
+                        onChange={(e) => setPhotoAuth(e.target.value)}
+                    />
+                    <label htmlFor="photoAuth">Autorização de fotos e vídeos da criança</label>
                 </div>
 
                 <label htmlFor="authorizedPeople">Lista de autorizados para buscar a criança:</label>
-                <input type="text" id="authorizedPeople" name="authorizedPeople" />
+                <input
+                    type="text"
+                    id="authorizedPeople"
+                    name="authorizedPeople"
+                    onChange={(e) => setAuthorizedPeople(e.target.value)}
+                />
 
                 <label htmlFor="classNumber">Turma:</label>
-                <select name="classNumber" value="">
+                <select
+                    name="classNumber"
+                    value={classNumber}
+                    onChange={(e) => setClassNumber(e.target.value)}
+                >
                     <option value="none"></option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -56,7 +159,12 @@ const StudentForm = () => {
                 </select>
 
                 <label htmlFor="additionalObs">Observações adicionais:</label>
-                <input type="text" id="additionalObs" name="additionalObs" />
+                <input
+                    type="text"
+                    id="additionalObs"
+                    name="additionalObs"
+                    onChange={(e) => setAdditionalObs(e.target.value)}
+                />
 
                 <button type="submit">Cadastrar</button>
             </div>
